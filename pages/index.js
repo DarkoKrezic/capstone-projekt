f4eb07748282475819b3fc6a7b6f24a0bb39ec6b
DarkoKrezic <darkotronic@gmail.com>
2023-05-10 10:44:05 +0200

    list fix

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,25 @@ import StoryList from "@/components/StoryList";
 import stories from "@/public/db";
 import styled from "styled-components";
 import Link from "next/link";
-
+import { useState, useEffect } from "react";
 const Button = styled.button`
   margin-bottom: 1rem;
 `;
 
 export default function HomePage() {
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    const storiesFromLocalStorage = JSON.parse(localStorage.getItem("stories"));
+    if (storiesFromLocalStorage) {
+      setStories(storiesFromLocalStorage);
+    } else {
+      setStories(stories);
+      localStorage.setItem("stories", JSON.stringify(stories));
+    }
+    console.log(storiesFromLocalStorage);
+  }, []);
+
   return (
     <>
       <StoryList stories={stories} />
