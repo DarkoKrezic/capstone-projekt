24546d4caf8b634df7df422aac6765facd0af3d5
DarkoKrezic <darkotronic@gmail.com>
2023-05-12 10:35:26 +0200

    using immer

--- components/StoryList/index.js
+++ components/StoryList/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 import {
   Header,
   StoryListContainer,
@@ -8,26 +8,28 @@ import {
   StoryCoverImage,
   StoryCreationDate,
 } from "./StyledStoryList";
+import { useImmerLocalStorageState } from "@/lib/hook/useImmerLocalStorageState";
 
-import initialStories from "@/public/db";
+import initialStories from "@/public/db.js";
 
 function StoryList() {
-  const [stories, setStories] = useState([]);
-
-  useEffect(() => {
+  const [stories, setStories] = useImmerLocalStorageState("stories", {
+    defaultValue: [],
+  });
+  if (typeof window !== "undefined") {
     const storedStories = JSON.parse(localStorage.getItem("stories"));
-
-    if (storedStories && storedStories.length > 0) {
-      setStories(storedStories);
-    } else {
+    if (!storedStories || storedStories.length === 0) {
       localStorage.setItem("stories", JSON.stringify(initialStories));
       setStories(initialStories);
     }
-  }, []);
+  }
+
+  console.log("stories:", stories);
 
   return (
     <StoryListContainer>
       <Header>My Stories</Header>
+
       {stories.map((story) => (
         <Link key={story.id} href={`/stories/${story.id}`}>
           <StoryListCard>
