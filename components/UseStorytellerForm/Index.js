cb33970ff931f8767f6c90a5d09a4794688f623f
DarkoKrezic <darkotronic@gmail.com>
2023-09-28 20:33:16 +0200

    prompt fix

--- components/UseStorytellerForm/Index.js
+++ components/UseStorytellerForm/Index.js
@@ -31,7 +31,7 @@ export default function UseStorytellerForm() {
       return;
     }
     const storyObjectPrompt = {
-      prompt: `You enact AESOP the great greek Storyteller. Tell a story for children in the style of AESOP in German language. It should contain a moral message. The response should be a JSON object, with the following properties: title: write here the title of the story, textContent: write the story text (max 600 words) using this following prompt as brief description: ${prompt}, coverImagePrompt: write the CoverImagePrompt (max-length 900 characters) that describes the story and add children book style at the end so we use that style to generate our image. Also do not use characters or  symbols that could cause syntax errors when parsing.`,
+      prompt: `Du bist ein Kindergeschichten Erzähler. Erzähle eine Geschichte basierend auf dem prompt : "${prompt}". Zunächst schreibst Du die Geschichte selbst ca.400 Wörter. Dann überlegst Du einen guten Titel für die Geschichte und überlegst Dir, wie man die Geschichte als ein Bild beschreiben könnte. Dein Response soll ein JSON Objekt mit keys so wie dieses: "title":"hier kommt der Titel der Geschichte", "textContent":"hier kommt die Geschichte", "coverImagePrompt":"hier kommt unsere Bildbeschreibung, und füge Kinderbuch Style am Ende hinzu"`,
     };
     try {
       setIsLoading(true);
